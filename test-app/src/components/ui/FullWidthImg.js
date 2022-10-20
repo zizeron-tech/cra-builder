35bf4d33bf6073c0eb46f5c6cdb172e36bcdc80e
eugeniozizeron <eugenio@zizeron.tech>
2022-10-20 10:53:02 +0200

    IAMLIB-10 remove test-app

--- test-app/src/components/ui/FullWidthImg.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const FullWidthImg = styled.img`
-  max-width: 100%;
-  margin: auto;
-`;
-
-export default FullWidthImg;
--- test-app/src/theme/variables.js
+++
@@ -1,5 +0,0 @@
-export const variables = {
-  fonts: {
-    primaryFont: `"Lato", sans-serif`,
-  }
-};
