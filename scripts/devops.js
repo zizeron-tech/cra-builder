ba8d715153e584d32fdd7564aaa83f51b09a6924
eugeniozizeron <eugenio@zizeron.tech>
2022-10-24 15:22:14 +0200

    change npm to yarn

--- scripts/devops.js
+++ scripts/devops.js
@@ -1,6 +1,6 @@
 fs = require('fs');
 
-const startLocal = 'npm run start:local';
+const startLocal = 'yarn start:local';
 
 fs.writeFile('./start-local.sh', startLocal, function(error) {
   if (error) console.log("error", error)
@@ -8,14 +8,14 @@ fs.writeFile('./start-local.sh', startLocal, function(error) {
 });
 
 
-const startStaging = 'npm run start:staging';
+const startStaging = 'yarn start:staging';
 
 fs.writeFile('./start-staging.sh', startStaging, function(error) {
   if (error) console.log("error", error)
   else console.log("Staging start script written"); 
 });
 
-const startProd = 'npm run start:prod';
+const startProd = 'yarn start:prod';
 
 fs.writeFile('./start-prod.sh', startProd, function(error) {
   if (error) console.log("error", error)
